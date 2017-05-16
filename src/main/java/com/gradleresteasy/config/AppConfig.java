2fa6887085ff84ebe731fce001802cc74c273036
Vali Shah <shaikanjavali.mastan@a-cti.com>
2017-05-17 02:37:50 +0530

    Added AppConfig to register endpoints , implemented a sample endpoint /hello

--- src/main/java/com/gradleresteasy/config/AppConfig.java
+++ src/main/java/com/gradleresteasy/config/AppConfig.java
@@ -1,10 +1,24 @@
 package com.gradleresteasy.config;
 
 
+import com.gradleresteasy.controller.SampleController;
+import org.apache.james.mime4j.storage.DefaultStorageProvider;
+import org.apache.james.mime4j.storage.MemoryStorageProvider;
+
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by valishah on 5/16/17.
  */
 public class AppConfig extends Application{
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+
+//        DefaultStorageProvider.setInstance(new MemoryStorageProvider());
+        classes.add(SampleController.class);
+        return classes;
+    }
 }
--- src/main/java/com/gradleresteasy/controller/SampleController.java
+++ src/main/java/com/gradleresteasy/controller/SampleController.java
@@ -1,6 +1,11 @@
 package com.gradleresteasy.controller;
 
+import javax.jdo.annotations.Query;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by valishah on 5/16/17.
@@ -8,4 +13,10 @@ import javax.ws.rs.Path;
 @Path("/api/v1")
 public class SampleController {
 
+
+    @GET
+    @Path("/hello")
+    public Response sayHello(@QueryParam("name") String name){
+        return Response.status(200).entity("Hello "+(name!=null?name:"World")+"!").build();
+    }
 }
