fff4eb5a98dc1e9c02c4e3b3fa4bf2700c65294c
Vali Shah <shaikanjavali.mastan@a-cti.com>
2017-05-17 01:56:40 +0530

    Included basic gradle configuration with appengine and resteasy

---
+++ src/main/java/com/gradleresteasy/api/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.gradleresteasy.api.config;
+
+
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by valishah on 5/16/17.
+ */
+public class AppConfig extends Application{
+}
---
+++ src/main/java/com/gradleresteasy/api/controller/SampleController.java
@@ -0,0 +1,11 @@
+package com.gradleresteasy.api.controller;
+
+import javax.ws.rs.Path;
+
+/**
+ * Created by valishah on 5/16/17.
+ */
+@Path("/api/v1")
+public class SampleController {
+
+}
